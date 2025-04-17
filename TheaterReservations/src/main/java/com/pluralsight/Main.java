567ef041e60147b916ce1b903ce7c48bc40447c0
ABrooksGit <Abrooks6211996@gmail.com>
2025-04-17 19:04:06 -0400

    TheaterReservations

--- TheaterReservations/src/main/java/com/pluralsight/Main.java
+++ TheaterReservations/src/main/java/com/pluralsight/Main.java
@@ -2,8 +2,6 @@ package com.pluralsight;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
-
-
 public class Main {
     public static void main(String[] args) {
 
@@ -11,36 +9,32 @@ public class Main {
         DateTimeFormatter formatter;
 
         System.out.println("Please enter your name: ");
-        String name = scanner.nextLine();
+        String name = scanner.nextLine().trim();
+
+        int firstSpaceIndex = name.indexOf(" ");
+        String firstName = name.substring(0, firstSpaceIndex);
+        String lastName = name.substring(firstSpaceIndex + 1);
+
 
         System.out.println("What date will you be coming(MM/dd/yyyy): ");
         String dateComing = scanner.nextLine();
         formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
         LocalDate event = LocalDate.parse(dateComing, formatter);
 
+
         System.out.println("How many tickets would you like? ");
-        String ticketAmount;
-        ticketAmount = scanner.nextLine();
+        int ticketAmount = scanner.nextInt();
 
-        if (ticketAmount.equals("1")){
 
-            System.out.println(ticketAmount + " ticket Reserved for " + dateComing + " Under " + name );
-        } else if(ticketAmount.equals("0")){
+        if (ticketAmount == 1) {
+
+            System.out.println(ticketAmount + " ticket Reserved for " + dateComing + " Under " + lastName + "," + firstName);
+        } else if(ticketAmount == 0){
             System.out.println("Incorrect");
         } else{
-            System.out.println(ticketAmount + " tickets Reserved for " + dateComing + " Under " + name );
+            System.out.println(ticketAmount + " ticket Reserved for " + dateComing + " Under " + lastName + "," + firstName);
         }
 
 
-
-
-
-
-
-
-
-
-
-
+        }
     }
-}
