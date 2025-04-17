b9874c3e1186c139acb605f48cc8edd70ae78f41
ABrooksGit <Abrooks6211996@gmail.com>
2025-04-17 14:45:19 -0400

    FullNameParser

---
+++ FullNameParser/src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //establish known values
+        System.out.println("What is your full name?: ");
+        String name = scanner.nextLine().trim();
+        
+        //calculate the unknown
+        int firstSpaceIndex = name.indexOf(" ");
+        String firstName = name.substring(0, firstSpaceIndex);
+        int secondSpaceIndex = name.indexOf(" ", firstSpaceIndex + 1 );
+
+        String middleName;
+        String lastName;
+
+
+        if(secondSpaceIndex >= 0){
+            //middle last
+            middleName = name.substring(firstSpaceIndex + 1, secondSpaceIndex);
+            lastName = name.substring(secondSpaceIndex + 1);
+
+
+        } else {
+            // only last name
+            middleName ="(None)";
+            lastName = name.substring(firstSpaceIndex +1);
+        }
+
+        //display the results
+        System.out.println();
+        System.out.println("First Name: " + firstName);
+        System.out.println("Middle Name: " + middleName);
+        System.out.println("Last Name: " + lastName);
+
+
+
+    }
+}
